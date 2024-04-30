88ad17a5cfc568d32732fb58d2b36ebe64292974
Steve <stevedsimkins@gmail.com>
2024-04-30 07:10:56 -0400

    fix

--- config/fdk.ts
+++ config/fdk.ts
@@ -2,7 +2,7 @@ import { PinataFDK } from "pinata-fdk";
 
 export const fdk = new PinataFDK({
   pinata_jwt: process.env.PINATA_JWT as string,
-  pinata_gateway: `${process.env.NEXT_PUBLIC_GATEWAY_URL}`,
+  pinata_gateway: `${process.env.NEXT_PUBLIC_GATEWAY}`,
   app_fid: `${process.env.DEVELOPER_FID}`,
   app_mnemonic: `${process.env.DEVELOPER_MNEMONIC}`
 });
